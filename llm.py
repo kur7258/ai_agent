32d678bda73012f51ce2651706a6e85993f46ed8
kur <tjrwls6011@naver.com>
2025-06-12 16:18:07 +0900

    Add: few shot

--- llm.py
+++ llm.py
@@ -8,7 +8,9 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import FewShotChatMessagePromptTemplate
 
+from config import answer_examples
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -56,6 +58,17 @@ def get_rag_chain():
     retriever = get_retriever()
     llm = get_llm()  
     
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ('human', '{input}'),
+            ('ai', '{answer}')
+        ]
+    )
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt = example_prompt,
+        examples = answer_examples
+    )
+    
     contextualize_q_system_prompt = (
         'Give a caht history and the latest user question '
         'which might reference context in the caht history, '
@@ -78,11 +91,11 @@ def get_rag_chain():
     )
 
     system_prompt = (
-        'You are an assistant for question-answering tasks. '
-        'Use the following pieces of retrieved context to answer '
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
+        "당신은 소득세법 전문가입니다. 사용자의 소득세법에 관한 질문에 답변해주세요"
+        "아래에 제공된 문서를 활용해서 답변해주시고"
+        "답변을 알 수 없다면 모른다고 답변해주세요"
+        "답변을 제공할 때는 소득세법 (XX조)에 따르면 이라고 시작하면서 답변해주시고"
+        "2-3 문장 정도의 짧은 내용의 답변을 원합니다"
         "\n\n"
         "{context}"
     )
@@ -90,6 +103,7 @@ def get_rag_chain():
     qa_prompt = ChatPromptTemplate.from_messages(
         [
             ('system', system_prompt),
+            few_shot_prompt,
             MessagesPlaceholder(variable_name='chat_history'),
             ('human', '{input}')
         ]
